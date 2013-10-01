fa01ebc7d97899fb4bd22aece792035918319c4d
Peter Lohmann <peter@lohmannhannover.de>
2013-10-02 00:06:04 +0200

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,6 @@ class ApplicationController < ActionController::Base
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
       username == "ichthys" && password == "testpasswort"
-    end if Rails.env == "deployment"
+    end if Rails.env == "production"
   end
 end
