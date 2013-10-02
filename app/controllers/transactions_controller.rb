c51d1ab08d674fc6aca0da86deb95ea618686bec
Peter Lohmann <peter@lohmannhannover.de>
2013-10-02 20:42:49 +0200

    wip

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,7 +3,7 @@ class TransactionsController < ApplicationController
 
   # GET /transactions
   def index
-    @transactions = Transaction.all
+    @transactions = Transaction.order("created_at desc")
   end
 
   # GET /transactions/1
