44e03fc6d2b4177497f67793e4444eef307ad143
Peter Lohmann <peter@lohmannhannover.de>
2014-04-30 21:34:32 +0200

    revert: transaction view caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,4 @@ Basar::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
-  config.action_controller.perform_caching = true
 end
