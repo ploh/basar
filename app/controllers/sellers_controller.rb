d51f49d84d91631c29eb8d55debdc4745123d08e
Peter Lohmann <peter@lohmann-online.info>
2014-11-21 16:22:59 +0100

    let devise mail stuff

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,4 +1,5 @@
 class SellersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_seller, only: [:show, :edit, :update, :destroy]
 
   # GET /sellers
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,10 +14,23 @@ Basar::Application.configure do
   config.action_controller.perform_caching = false
 
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      openssl_verify_mode: 'none',
+      user_name: "peterlohmann2@googlemail.com",
+      password: File.readlines( "#{ File.dirname( __FILE__ ) }/../secret/mail_password.dat" ).first.chomp
+  }
+
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
 
   # Print deprecation notices to the Rails logger.
