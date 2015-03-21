6be101f353df5a52a65a67222599759fe7fd614e
Peter Lohmann <peter@lohmann-online.info>
2015-03-21 14:07:45 +0100

    mock_sign_in for controller rspec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe PagesController, :type => :controller do
 
   describe "GET home" do
     it "returns http success" do
+      mock_sign_in nil
       get :home
       expect(response).to have_http_status(:success)
     end
---
+++ spec/support/controller_helpers.rb
@@ -0,0 +1,18 @@
+module ControllerHelpers
+  def mock_sign_in(user = double('user'))
+    if user.nil?
+      # does not work somehow
+#      allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, {:scope => :user})
+      allow(request.env['warden']).to receive(:authenticate!).and_return(nil)
+      allow(controller).to receive(:current_user).and_return(nil)
+    else
+      allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+      allow(controller).to receive(:current_user).and_return(user)
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include Devise::TestHelpers, :type => :controller
+  config.include ControllerHelpers, :type => :controller
+end
