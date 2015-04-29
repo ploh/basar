800eb90835474efe87518a0e25c5c374e18bf477
Peter Lohmann <peter@lohmann-online.info>
2015-04-29 16:31:04 +0200

    generate and show client id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,16 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
+
+  before_action :get_or_create_client_key
+
+
+  private
+
+  def get_or_create_client_key
+    @client_key = cookies.permanent.signed[:client_key]
+    unless @client_key
+      @client_key = cookies.permanent.signed[:client_key] = Digest::SHA1.hexdigest([request.remote_ip, request.user_agent, rand].to_s)[0..3]
+    end
+  end
 end
