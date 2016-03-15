18766bc0e52dd10974c4bf1fe6bf8c1750b73fe5
Peter Lohmann <peter@lohmann-online.info>
2016-03-15 14:19:13 +0100

    symbols for enum values please

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Basar::Application.configure do
       address: "smtp.gmail.com",
       port: 587,
       domain: "gmail.com",
-      authentication: "plain",
+      authentication: :plain,
       enable_starttls_auto: true,
       openssl_verify_mode: 'none',
       user_name: "peterlohmann2@googlemail.com",
