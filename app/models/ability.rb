f67af711303b1e25b3af89267fd10c4e9f8e729a
Peter Lohmann <peter@lohmann-online.info>
2016-03-25 12:26:35 +0100

    no exception on authorization check if not logged in

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -28,12 +28,14 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
-    case user.role
-    when 'admin'
-      can :manage, :all
-    when 'assistant'
-      can :manage, Transaction
-      can :read, :all
+    if user
+      case user.role
+      when 'admin'
+        can :manage, :all
+      when 'assistant'
+        can :manage, Transaction
+        can :read, :all
+      end
     end
   end
 end
