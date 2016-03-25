505d79dcdd8a122755e821755cc3bb78757f6554
Peter Lohmann <peter@lohmann-online.info>
2016-03-25 12:14:09 +0100

    some real user administration
    - editing user role
    - deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  load_and_authorize_resource
+
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -6,34 +8,14 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  # GET /users/1
-  def show
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
   # GET /users/1/edit
   def edit
-  end
-
-  # POST /users
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
-    else
-      render :new
-    end
   end
 
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
+      redirect_to users_url, notice: 'User was successfully updated.'
     else
       render :edit
     end
