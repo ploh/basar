86660d7b8674f9a5ad6b11383972d7acd5983e41
Peter Lohmann <peter@lohmann-online.info>
2016-03-26 17:51:37 +0100

    move configuration to corresponding comment
    - almost forgot that it was already configured

--- config/application.rb
+++ config/application.rb
@@ -15,15 +15,13 @@ module Basar
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = "Berlin"
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
     config.sass.preferred_syntax = :sass
-
-    config.time_zone = "Berlin"
 
     config.action_view.field_error_proc = Proc.new do |html_tag, instance|
       if html_tag.start_with?("<label")
