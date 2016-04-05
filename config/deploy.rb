252745e4a6927d9fcac172b6164466b1920d9092
Peter Lohmann <peter@lohmann-online.info>
2016-04-05 12:05:33 +0200

    capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,16 @@
-# config valid only for current version of Capistrano
-lock '3.4.0'
+# configured according to https://www.phusionpassenger.com/library/deploy/apache/automating_app_updates/ruby/
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+# config valid only for current version of Capistrano
+# lock '3.4.0'
+
+set :application, 'basar'
+set :repo_url, 'peter@kids-basar.de:kids-basar.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/www/vhosts/kids-basar.de/app'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -26,16 +28,19 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 50
+
+set :rbenv_type, :system
+set :rbenv_ruby, '2.1.2'
+
 
 namespace :deploy do
-
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
@@ -44,5 +49,4 @@ namespace :deploy do
       # end
     end
   end
-
 end
