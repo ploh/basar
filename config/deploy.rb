b245c86ad469d8e8af86f210ceada4f399e1919a
Peter Lohmann <peter@lohmann-online.info>
2016-04-05 12:55:54 +0200

    keep database during deployments

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,7 @@ set :deploy_to, '/var/www/vhosts/kids-basar.de/app'
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('db/production.sqlite3')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
