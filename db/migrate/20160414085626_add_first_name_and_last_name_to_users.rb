6ad25a060ce08acc044c0bc13f4f603eeb4c7729
Peter Lohmann <peter@lohmann-online.info>
2016-04-14 10:59:41 +0200

    initialize first and last name with old name

--- db/migrate/20160414085626_add_first_name_and_last_name_to_users.rb
+++ db/migrate/20160414085626_add_first_name_and_last_name_to_users.rb
@@ -1,6 +1,12 @@
 class AddFirstNameAndLastNameToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
+    add_column :users, :first_name, :string, null: false, default: ""
+    add_column :users, :last_name, :string, null: false, default: ""
+
+    User.all.each do |user|
+      user.first_name = user.name
+      user.last_name = user.name
+      user.save
+    end
   end
 end
