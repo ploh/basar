b309b634f15bb400be118a2964eba0265ca33cdb
Peter Lohmann <peter@lohmann-online.info>
2016-04-14 11:06:44 +0200

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414085626) do
+ActiveRecord::Schema.define(version: 20160414090533) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "seller_id",                   null: false
@@ -83,7 +83,6 @@ ActiveRecord::Schema.define(version: 20160414085626) do
     t.datetime "updated_at"
     t.integer  "role"
     t.string   "old_seller_code"
-    t.string   "name",                               default: "", null: false
     t.integer  "seller_model"
     t.string   "first_name",                         default: "", null: false
     t.string   "last_name",                          default: "", null: false
