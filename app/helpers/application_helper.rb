d1addb9d30a426ccae2da0e991b6a0b2d1a8c62b
Peter Lohmann <peter@lohmann-online.info>
2016-04-16 19:31:54 +0200

    refactor user sign up and edit into common form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def money_string(amount)
     sprintf("%1.2f EUR", amount).gsub(".", ",")
   end
+
+  def t_main key, *args
+     I18n.t "#{params[:controller].gsub('/', '.')}.#{params[:action]}.#{key}", *args
+  end
 end
