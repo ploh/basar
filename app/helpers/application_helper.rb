3ad0435771db7b7bc7340d3edbc955b5653b7082
Peter Lohmann <peter@lohmann-online.info>
2016-04-17 07:41:13 +0200

    repair i18n for user registration forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,4 @@ module ApplicationHelper
   def money_string(amount)
     sprintf("%1.2f EUR", amount).gsub(".", ",")
   end
-
-  def t_main key, *args
-     I18n.t "#{params[:controller].gsub('/', '.')}.#{params[:action]}.#{key}", *args
-  end
 end
