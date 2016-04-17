14cc187c3fe4da84aab95a436141cad35ebcc399
Peter Lohmann <peter@lohmann-online.info>
2016-04-17 22:46:49 +0200

    Revert "better symlink complete db"
    This reverts commit 9be3780ef90cfbd5e013b561b4e298e8ebf5342e.
    
    this was very stupid in the first place because it e.g. removes all migrations

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,9 +26,10 @@ set :deploy_to, '/var/www/vhosts/kids-basar.de/app'
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('db/production.sqlite3', 'db/production.sqlite3-shm', 'db/production.sqlite3-wal')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('db', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
