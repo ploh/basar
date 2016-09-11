cb9b9306c9e90f088bb2fa83bf8a8f06c86459ec
Peter Lohmann <peter@lohmann-online.info>
2016-09-11 11:41:07 +0200

    validate activity counts

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,7 +2,8 @@ class Activity < ActiveRecord::Base
   belongs_to :seller, touch: true
   belongs_to :task
 
-  validates :planned_count, inclusion: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  validates :planned_count, inclusion: (0..12).to_a
+  validates :actual_count, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   def task_description
     task && task.description
