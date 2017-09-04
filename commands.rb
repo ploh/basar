7af9df02d992f10c51a97cc7155486035c4a562d
Peter Lohmann <hi@ploh.io>
2017-09-04 20:14:56 +0200

    More useful console commands

--- commands.rb
+++ commands.rb
@@ -3,3 +3,7 @@ Transaction.all.group_by {|t| t.items.map {|i| [i.price.to_f, i.seller_id]}.sort
 
 # List sellers by decreasing revenue
 puts Seller.all.sort_by {|s| -s.total_revenue}.first(50).map {|s| sprintf "%6s  %6.2f  %s", s.code, s.total_revenue, s.name}.join("\n"); 0
+
+# Delete all transactions and sellers - good after loading a backup in the application phase
+Transaction.all.each {|t| t.destroy}
+Seller.all.each {|s| s.destroy}
