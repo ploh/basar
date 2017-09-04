7d3455e5fd36f661447a70fc2823dff7b67a5d7f
Peter Lohmann <hi@ploh.io>
2017-09-04 15:47:59 +0200

    rails console command for listing best sellers

--- commands.rb
+++ commands.rb
@@ -1,1 +1,5 @@
+# Find duplicate transactions - probably erroneously entered twice
 Transaction.all.group_by {|t| t.items.map {|i| [i.price.to_f, i.seller_id]}.sort}.reject {|i,t| t.size == 1}.each {|i,ts| ts.each {|t| p t}; puts}; 0
+
+# List sellers by decreasing revenue
+puts Seller.all.sort_by {|s| -s.total_revenue}.first(50).map {|s| sprintf "%6s  %6.2f  %s", s.code, s.total_revenue, s.name}.join("\n"); 0
